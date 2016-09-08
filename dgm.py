28939ac8aac95fbaf2086515f923f3b465b3ceec
rlreed <rlreed@ksu.edu>
2016-09-08 09:24:17 -0500

    Adding ability to use different basis sets.  Only DLP and DCT work correctly

--- dgm.py
+++ dgm.py
@@ -1,9 +1,21 @@
 from __future__ import division
 import numpy as np
 import numpy.linalg as LA
+import matplotlib.pyplot as plt
+from matplotlib import rc
+rc('font',**{'family':'serif'})
+from matplotlib import rcParams
+rcParams['xtick.direction'] = 'out'
+rcParams['ytick.direction'] = 'out'
+rcParams['xtick.labelsize'] = 18
+rcParams['ytick.labelsize'] = 18
+rcParams['lines.linewidth'] = 1.85
+rcParams['axes.labelsize'] = 20
+rcParams.update({'figure.autolayout': True})
+from numpy.polynomial import Chebyshev
 
 class solver(object):
-    def __init__(self, sig_t, sig_s, vsig_f, chi, phi, k=1, lamb=1, basis='dlp'):
+    def __init__(self, sig_t, sig_s, vsig_f, chi, phi, k=1, lamb=1, basis='DLP', silent=True):
         self.sig_t = sig_t
         self.sig_s = sig_s
         self.vsig_f = vsig_f
@@ -13,16 +25,18 @@ class solver(object):
         self.lamb = lamb
         self.ep = 1e-8
         self.basisType = basis
+        self.silent = silent
         
         # Number of groups
         self.G = len(self.sig_t)
         
         self.getBasis()
         self.calcCrossSections()
+#         self.solve()
         
     def calcCrossSections(self):
         # Get total cross section
-        self.Sig_t = np.sum(self.sig_t * self.phi) / np.sum(self.phi) 
+        self.Sig_t = np.sum(self.basis[0] * self.sig_t * self.phi) / np.sum(self.basis[0] * self.phi) 
         # Assume isotropic flux, i.e. psi = phi / (4\pi)
         self.delta = self.basis.dot((self.sig_t - self.Sig_t) * self.phi) / np.sum(self.phi)
         
@@ -39,8 +53,21 @@ class solver(object):
 
         
     def getBasis(self):
-        if self.basisType == 'dlp':
+        if self.basisType == 'DLP':
             self.DLP()
+        elif self.basisType == 'mDLP':
+            self.mDLP()
+        elif self.basisType == 'cheb':
+            self.cheb()
+        elif self.basisType == 'dct':
+            self.DCT()
+            
+    def DCT(self):
+        self.basis = np.zeros((self.G, self.G))
+        for i in range(self.G):
+            for j in range(self.G):
+                self.basis[j,i] = np.cos(np.pi / self.G * (i + 0.5) * j)
+    
         
     def DLP(self):
         # initialize all functions to unity 
@@ -62,6 +89,18 @@ class solver(object):
 #         self.basis, _ = LA.qr(self.basis, 'full')
 #         # Structure so that self.basis[1] provides a vector of the linear function
 #         self.basis = self.basis.T
+
+    def mDLP(self):
+        self.DLP()
+        pattern = np.array([1, 1])
+        for i in range(len(self.basis)):
+            self.basis[i] *= pattern
+            
+#         # Orthogonalize the basis functions
+#         self.basis, _ = LA.qr(self.basis, 'full')
+#         # Structure so that self.basis[1] provides a vector of the linear function
+#         self.basis = self.basis.T
+        
         
     def update(self):
         # Update k
@@ -82,7 +121,8 @@ class solver(object):
         ep = 1
         if self.G == 2:
             while True:
-                print 'iteration = {:3}, f = {:12.10f}, k = {:12.10f}, eps = {:12.10f}'.format(it, self.phi[1], self.k, ep)
+                if not self.silent:
+                    print 'iteration = {:3}, f = {:12.10f}, k = {:12.10f}, eps = {:12.10f}'.format(it, self.phi[1], self.k, ep)
                 old = self.phi[1]
                 self.calcCrossSections()
                 self.update()
@@ -97,7 +137,6 @@ class solver(object):
         print self.delta
         print self.Sig_s
         print self.vSig_f
-#         print self.Chi
 
 class twoGroupSolver(object):
     def __init__(self, sig_t, sig_s, vsig_f, f=1, k=1, lamb=1):
@@ -152,11 +191,29 @@ class twoGroupSolver(object):
         print self.vSig_f
         
 if __name__ == '__main__':
-    sig_t = np.array([1,2])
+    N = 50
+    sig_t = np.array([1,3])
     sig_s = np.array([[0.3, 0.3],
                       [0, 0.3]])
     vsig_f = np.array([0.5, 0.5])
     chi = np.array([1,0])
-    phi = np.array([1,1])
-    S = solver(sig_t, sig_s, vsig_f, chi, phi)
-    S.solve()
+    phi = np.array([1,0.05])
+    input = np.linspace(0,0.5,N)
+    fs = np.zeros(N)
+    for i, f in enumerate(input):
+        phi = np.array([1, f])
+        S = solver(sig_t, sig_s, vsig_f, chi, phi, basis='DLP')
+        S.update()
+        fs[i] = S.phi[1] / S.phi[0]
+        print S.phi[1] / S.phi[0]
+    
+    plt.plot(input, fs, 'b-')
+    plt.plot(input, input, 'k--')
+    plt.plot((input[1:] + input[:-1]) / 2.0, (fs[1:] - fs[:-1]) / (input[1:] - input[:-1]), 'r--')
+    plt.ylim(-1.5, 1.0)
+    plt.xlabel('input')
+    plt.ylabel('output')
+    plt.legend(['f(x)', 'y=x', 'f\'(x)'], loc=4)
+    plt.xticks(np.linspace(0,0.5,11))
+    plt.grid()
+    plt.savefig('dlp3.pdf')
